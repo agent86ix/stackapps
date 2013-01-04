418619969a0c8235a98e189e2d63505f0912c6ef
agent86ix <agent86ix@gmail.com>
2013-01-04 19:09:24 +0300

    adding pro-forma comments script for remote download.

---
+++ proforma/pf.js
@@ -0,0 +1,32 @@
+callback(
+[
+{ "name": "Prohibited ITG", "description": "This is a question that falls under the category of \"requests for game identification,\" which is prohibited on the site.  Please consult the <a href=\"http://$SITEURL$/faq\">FAQ</a> if you're not sure if a question is allowed.  You might find <a href=\"http://blog.gaming.stackexchange.com/2012/08/identifying-games-when-you-cant-remember-the-name/\">this blog post</a> helpful as well."},
+
+{ "name": "Prohibited ShopRec", "description": "This is a question that falls under the category of \"shopping advice and recommendations,\" which is prohibited on the site.  Please consult the <a href=\"http://$SITEURL$/faq\">FAQ</a> if you're not sure if a question is allowed.  You might find <a href=\"http://blog.gaming.stackexchange.com/2012/11/shopping-advice-for-gamers/\">this blog post</a> helpful as well."},
+
+{ "name": "Prohibited Catalog", "description": "This is a question that falls under the category of \"catalogues - listing games that fit specific criteria or are like an existing game,\" which is prohibited on the site.  Please consult the <a href=\"http://$SITEURL$/faq\">FAQ</a> if you're not sure if a question is allowed."},
+
+{ "name": "Prohibited Speculative", "description": "This is a question that falls under the category of \"speculation of the future of the industry and of upcoming releases,\" which is prohibited on the site.  Please consult the <a href=\"http://$SITEURL$/faq\">FAQ</a> if you're not sure if a question is allowed."},
+
+{ "name": "Prohibited Piracy", "description": "This is a question that falls under the category of hacking, piracy, and other violations of EULA or legal guidelines, which is prohibited on the site.  "},
+
+{ "name": "Prohibited GameDev", "description": "This is a question that falls under the category of \"game and mod development,\" which may be a better fit for our sister site, <a href=\"http://gamedev.stackexchange.com/\">GameDev</a>.  This question may be migrated at a moderator's discretion.  "},
+
+{ "name": "OP using an answer for further information", "description": "Please use the <em>Post answer</em> button only for actual answers. You should modify your original question to add additional information."},
+
+{ "name": "Link-only answer", "description": "While this link may theoretically answer the question, <a href=\"http://meta.stackoverflow.com/q/8259\">it would be preferable</a> to include the essential parts of the answer here, and provide the link for reference.  Be careful - don’t plagiarize the content behind the link!"},
+
+{ "name": "Dupe answer", "description": "This information has already been posted by another user.  Duplicated information <a href=\"http://$SITEURL$/faq#deletion\">may be removed</a> if it does not add anything to the existing answers."},
+
+{ "name": "Not an answer ", "description": "This space is reserved for answers to the question.  As this post doesn't belong here, it will be <a href=\"http://$SITEURL$/faq#deletion\">removed shortly.</a>"},
+
+{ "name": "Not an answer - comment", "description": "This space is reserved for answers to the question.  This will be converted to a comment. With <a href=\"http://$SITEURL$/privileges/comment\">sufficient rep</a>, you can post comments on other people's questions and answers. "},
+
+{ "name": "Not an answer - new question", "description": "This space is reserved for answers to the question.  If you have another question, please ask it by clicking the <a href=\"http://$SITEURL$/questions/ask\">Ask Question</a> button.  Make sure you include as much relevant detail as you can!"},
+
+{ "name": "Not an answer - 'me too'", "description": "This space is reserved for answers to the question.  If you’re having the same problem, with enough rep you can help this question get answered by <a href=\"http://$SITEURL$/privileges/vote-up\">upvoting</a> the question or <a href=\"http://$SITEURL$/faq#bounty\">offering a bounty.</a> Alternatively, \"star\" it as a favorite and you will be notified of any new answers."},
+
+{ "name": "Not an answer - 'thanks' ", "description": "This space is reserved for answers to the question.  Instead, with sufficient <a href=\"http://$SITEURL$/privileges/vote-up\">rep</a> you can upvote answers you like, which is the $SITENAME$ way of saying thank you."},
+
+]
+)
